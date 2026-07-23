c512ff64d1f3360aec4c0eb6b3c210ff90728203
goodbyegangster <goodbyegangster@outlook.jp>
2026-07-23 23:53:20 +0900

    add

--- dcm/sources/definitions/datacatalog/procedure/refresh_catalog.sql
+++ dcm/sources/definitions/datacatalog/procedure/refresh_catalog.sql
@@ -4,11 +4,12 @@
 define procedure {{ datacatalog_database_name }}.PROCEDURE.REFRESH_CATALOG()
     returns string
     language sql
-    comment = 'CATALOG テーブル洗替処理'
+    comment = 'CATALOG スキーマ向け procedure 実行処理'
     execute as owner
 as
 $$
 begin
+    -- 各処理の依存関係に従い順番に実行
     call {{ datacatalog_database_name }}.PROCEDURE.REFRESH_USERS();
     call {{ datacatalog_database_name }}.PROCEDURE.REFRESH_ASSETS();
     call {{ datacatalog_database_name }}.PROCEDURE.REFRESH_COLUMNS();
